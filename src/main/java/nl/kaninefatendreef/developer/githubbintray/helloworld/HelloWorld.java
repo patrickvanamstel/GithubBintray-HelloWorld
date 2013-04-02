dd38df5c9a331d1cd8c6bb5bae9cbdc8a36861df
patrickvanamstel <patrickvanamstel@gmail.com>
2013-04-02 07:54:49 +0200

    Class commit

---
+++ src/main/java/nl/kaninefatendreef/developer/githubbintray/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package nl.kaninefatendreef.developer.githubbintray.helloworld;
+
+public class HelloWorld {
+
+	public static void main (String [] args){
+		System.out.println("HelloWorld");
+	}
+	
+}
